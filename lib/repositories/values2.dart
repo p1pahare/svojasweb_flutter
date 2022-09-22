e72dc2e6488e07d393c41250f63a1a16205438dd
Parikshit Pahare <parpahar@publicisgroupe.net>
2022-09-22 11:46:49 +0530

    values added

--- lib/repositories/values2.dart
+++ lib/repositories/values2.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
-class Values {
+class Values2 {
   static const base_url = 'http://localhost:3000/api/';
   static const login = 'login';
   static const get_statistics = 'get_statistics';
